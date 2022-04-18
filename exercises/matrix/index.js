eb069af1bd6f89da26d7af66d2bf46daad107249
Maria Budzanowska <budzanowska.maria@gmail.com>
2022-04-18 20:21:36 +0200

    matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,41 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    arr[i] = [];
+  }
+
+  let counter = 1;
+  let row = 0;
+  let endRow = n - 1;
+  let col = 0;
+  let endCol = n - 1;
+  while (counter <= n * n) {
+    for (let c = col; c <= endCol; c++) {
+      arr[row][c] = counter;
+      counter++;
+    }
+    row++;
+    for (let r = row; r <= endRow; r++) {
+      arr[r][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+    for (let c = endCol; c >= col; c--) {
+      arr[endRow][c] = counter;
+      counter++;
+    }
+    endRow--;
+    for (let r = endRow; r >= row; r--) {
+      arr[r][col] = counter;
+      counter++;
+    }
+    col++;
+  }
+
+  return arr;
+}
 
 module.exports = matrix;
