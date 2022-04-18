1696c973ea63c303c8cdb86f4b8b64ae6e0e24bf
Maria Budzanowska <budzanowska.maria@gmail.com>
2022-04-18 13:44:01 +0200

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = 0;
+  let char = "";
+
+  [...str].reduce((prev, curr) => {
+    const newObj = { ...prev, [curr]: prev[curr] ? prev[curr] + 1 : 1 };
+    if (newObj[curr] > max) {
+      (char = curr), (max = newObj[curr]);
+    }
+    return newObj;
+  }, {});
+
+  return char;
+}
 
 module.exports = maxChar;
